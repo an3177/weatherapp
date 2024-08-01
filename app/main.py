2117856d58216d2111c17fa5024fb2a86f2e753e
Anjana <nairanjana696@gmail.com>
2024-08-01 09:36:28 -0700

    added some changes

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import users, weather
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -9,3 +10,11 @@ app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(weather.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
